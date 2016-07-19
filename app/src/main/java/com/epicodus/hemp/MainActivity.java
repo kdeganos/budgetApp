f1dc787a3dd30c0ea039fdecae3fddd394e77a65
Kevin Deganos and Ming Wen <mwen82@gmail.com>
2016-07-19 15:57:28 -0700

    Initial commit

---
+++ app/src/main/java/com/epicodus/hemp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.epicodus.hemp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PieChart pieChart = (PieChart) findViewById(R.id.chart);
+        // creating data values
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(12f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+
+        PieDataSet dataset = new PieDataSet(entries, "# of Calls");
+        dataset.setValueTextSize(12f);
+
+        // creating labels
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        PieData data = new PieData(labels, dataset); // initialize Piedata
+        pieChart.setData(data);
+
+        pieChart.setDescription("Description");  // set the description
+    }
+}
